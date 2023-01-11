52348f36fc4e137bbefcf5902570eb3456130664
在下猫南北 <rakiy@qq.com>
2023-01-11 19:59:12 +0800

    update 2.0.4

--- src/lhqw.ts
+++ src/lhqw.ts
@@ -2,13 +2,15 @@
 class Core {
 
   // 当前版本
-  readonly VERSION:string = "2.0.3";
+  readonly VERSION:string = "2.0.4";
 
   // 密钥默认分割符
   readonly Spt:string = "引"; 
 
   // 负号表示字符
   readonly Sign:string = "配";
+
+  readonly Step:number = 2;
   
   // 创建要用到加密的串,连花清瘟的配方一共14位药
   keys: Array<string> = ["连翘", "银花", "麻黄", "苦杏仁", "石膏", "板蓝根", "贯众", "鱼腥草", "红景天", "薄荷脑", "藿香", "大黄", "甘草", "淀粉"];
@@ -25,12 +27,12 @@ class Core {
     str = this._trim(str);
     if (str.length < 1) return '';
     // 这里生成默认的密钥索引，因为一共是0-13，但是因为是16转14，最小值应该至少是2
-    this.keyInd = Math.floor(Math.random() * 11) + 2;
+    this.keyInd = Math.floor(Math.random() * 11) + this.Step;
     // 用于装载转换后的utf8串
     let strUtf8:string = "";
     for(let s of str) {
       // 如果这些值在ascii范围，则直接转换
-      if (s.charCodeAt(0) < 128) {
+      if (s.charCodeAt(0) < 128 && s.charCodeAt(0) > 15) {
         strUtf8 += s.codePointAt(0)?.toString(16) || '';
       } else {
         // 否则则为UTF8
@@ -48,6 +50,7 @@ class Core {
       if (n < 0) ret += this.Sign;
       ret += this.keys[Math.abs(n)];
     }
+    
     // 然后把这些连起来，就是我们要的加密串啦
     return this.keys[this.keyInd] + this.Spt + ret;
   }
@@ -85,6 +88,7 @@ class Core {
     // 在这里直接开始把值转换好
     for (let i = 0; i < hex.length; i++) {
       ret += (odd ? '%' : '') + (Number.parseInt(hex[i]) + this.keyInd).toString(16).toUpperCase();
+      console.log(ret);
       odd = !odd;
     }
     // 返回结果
